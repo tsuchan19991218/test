82b029b9f7a6d15d2c6aab3c7eda354e93c57dff
tsuchan19991218 <ylwrvr.t.koga+git@gmail.com>
2021-04-12 02:12:39 +0900

    第三回コミット
    add C++ file

---
+++ c++/test.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (int)(n); i ++)
+
+vector <int> dy = {0, 1, 0, -1};
+vector <int> dx = {1, 0, -1, 0};
+
+const int INF = 1000000000;
+const ll INFLL = 100000000000000000;
+
+long long pow(long long x, long long n) {
+    long long ret = 1;
+    while (n > 0) {
+        if (n & 1) ret *= x;  // n の最下位bitが 1 ならば x^(2^i) をかける
+        x *= x;
+        n >>= 1;  // n を1bit 左にずらす
+    }
+    return ret;
+}
+
+int main(){
+    cout << "abc" << endl;
+}
